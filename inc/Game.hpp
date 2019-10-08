d9d27b2026ba1c8ae4e827c1c2b8abbaa07dd6cb
Wiktor <wiktor.lasota@vp.pl>
2019-10-08 17:47:30 +0200

    Chances, getter and setter

--- inc/Game.hpp
+++ inc/Game.hpp
@@ -3,8 +3,10 @@
 class Game
 {
 private:
-
+int chances_ {10};
 public:
     Game() = default;
+    int GetChances() {return chances_;}
+    void SetChances(int chances) {chances_ = chances}
     int PrintIntroduction();
 };
