6f7ae84515595052a7f768377d7c7fbc2fb4e2b2
Wiktor <wiktor.lasota@vp.pl>
2019-10-09 17:22:39 +0200

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,6 @@
 int main ()
 {
     Game g;
-    // g.PrintIntroduction();
 
-
-    auto res = testpin.ToString();
-    std::cout << res << "\n";
-    // auto result = testpin == anothertestpin;
-    // std::cout << result;
-    Pins collection;
-    collection.AddPin(testpin);
-    collection.AddPin(anothertestpin);
-    collection.PrintPins();
-
-
-    test.PrintPins();
-
-    auto result = test==collection;
-    std::cout << result;
     return 0;
 }
