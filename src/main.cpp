5da39316debd3633d341aa2b692e30407b7acd44
Wiktor <wiktor.lasota@vp.pl>
2019-10-11 19:55:23 +0200

    Generate random pins comination

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,25 @@
 #include "Pin.hpp"
 #include "Pins.hpp"
 #include "Combinations.hpp"
+#include <ctime>
 
+void GetRandomPins(Pins & pins)
+{
+    int random {};
+   
+    for (auto i = 0; i < 4; i++)
+    {
+        random = rand() % 4;
+        pins.AddPin(testCombination1.at(random));
+    }
+}
 
 int main ()
 {
+    srand(time(NULL));
     Game g;
     Pins pins;
-    for (auto const ele : testCombination1)
-    {
-        pins.AddPin(ele);
-    }
+    GetRandomPins(pins);
     pins.PrintPins();
     return 0;
 }
